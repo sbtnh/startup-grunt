99868db4e9d70edd6da45ca94a585c915a85ef5e
Sebastien Hiticas <sebastien.hiticas@gmail.com>
2017-01-08 11:45:37 -0500

    update task

--- grunt/kraken.js
+++ grunt/kraken.js
@@ -1,8 +1,8 @@
 module.exports = {
     kraken: {
         options: {
-            key: '40240f394855b2c5a5a8ea50351c5d01',
-            secret: 'dd145206068e8cd53b5eceff09890e06c9a9e0d8',
+            key: '',
+            secret: '',
             lossy: true
         },
         files: [{
